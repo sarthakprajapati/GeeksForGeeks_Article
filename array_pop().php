642a48049f698218a45b2eab3e5d5a911d3b026f
Sarthak Prajapati <sarthakprajapati@live.in>
2018-09-18 01:14:34 +0530

    array_pop , array_slice , array_values

---
+++ array_pop().php
@@ -0,0 +1,9 @@
+
+<?php 
+$array = array(
+    33 => 'geeks', 
+    36 => 'for', 
+    42 => 'computer'
+);
+echo array_pop(array_reverse($array));
+?>
---
+++ array_values().php
@@ -0,0 +1,9 @@
+
+<?php 
+$array = array(
+    33 => 'geeks', 
+    36 => 'for', 
+    42 => 'computer'
+);
+echo array_values($array)[0];
+?>
